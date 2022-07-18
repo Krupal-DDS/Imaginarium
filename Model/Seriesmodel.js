e000bb5e3b5a3bc753bb1d2e2b31dd93fea9898a
Krupal-DDS <krupal.dds@gmail.com>
2022-07-18 12:35:31 +0530

    first commit

---
+++ Model/Seriesmodel.js
@@ -0,0 +1,80 @@
+const mongoose = require('mongoose')
+
+const seriesdata = mongoose.Schema({    
+    files:[{
+        type: String,
+        require: true,
+    }],
+    title:{
+        type: String,
+        unique:true,
+        require: true
+    },
+    logline:{          
+        incident:{
+            type:String,            
+        },
+        protagonist:{
+            type: String,            
+        },
+        action: {
+            type: String
+        },
+        antagonist: {
+            type: String
+        }        
+    },
+    tagline: {
+        type: String,
+        require: true
+    },
+    synopsis: {
+        type: String,
+        require: true
+    },
+    actor:[{        
+        actorname: String,           
+        heroname:  String,            
+        tallent: String        
+    }],
+    author:{
+        type: mongoose.Schema.Types.ObjectId, ref:'Userdata',
+        require: true
+    },
+    tags: [
+        String
+    ],
+    
+    genres:[],
+    similar:[{
+        seriesId:String,
+        files:[String],
+        title:String,
+        synopsis:String,
+    }],
+    screenplay:[{
+        SeasonName:String,
+        Description:String,
+        Episodes:[{
+            EpName:String,
+            Act:[{
+                ActName:String,
+                Scene:[{
+                    SceneTitle:{type:String,default:'Please Enter Scene Title'},
+                    Description:{type:String,default:'Please Enter Description'},
+                    Actors:[{
+                        actorphoto:String,
+                        actorname:String,
+                        heroname:String,
+                        tallent:String
+                    }]
+                }]
+            }]
+        }]
+    }]
+ 
+})
+
+module.exports = mongoose.model("Series",seriesdata)
+
+
